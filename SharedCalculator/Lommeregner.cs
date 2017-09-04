4872e71d6fd18a2aeef3645567775e1bbfe04670
Benjamin Brobæk Lindgaard <benj2421@edu.easj.dk>
2017-09-04 13:57:56 +0200

    Lavede en lommeregner klasse med 5 metoder

---
+++ SharedCalculator/Lommeregner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator
+{
+    public class Lommeregner
+    {
+        /// <summary>
+        /// Dette er en metode til at lægge to tal sammen
+        /// </summary>
+        /// <param name="InputA"></param>
+        /// <param name="InputB"></param>
+        /// <returns></returns>
+        public double Addition(double InputA, double InputB)
+        {
+            return InputA + InputB;
+        }
+
+        /// <summary>
+        /// Dette er en metode til at trække to metoder fra hinanden 
+        /// </summary>
+        /// <param name="InputA"></param>
+        /// <param name="InputB"></param>
+        /// <returns></returns>
+        public double Subtraction(double InputA, double InputB)
+        {
+            return InputA - InputB;
+        }
+
+        /// <summary>
+        /// Dette er en metode til at gange to talt
+        /// </summary>
+        /// <param name="InputA"></param>
+        /// <param name="InputB"></param>
+        /// <returns></returns>
+        public double Multiplication(double InputA, double InputB)
+        {
+            return InputA * InputB;
+        }
+
+        /// <summary>
+        /// Dette er en metode til at dividerer to tal med hinanden
+        /// </summary>
+        /// <param name="InputA"></param>
+        /// <param name="InputB"></param>
+        /// <returns></returns>
+        public double Division(double InputA, double InputB)
+        {
+            return InputA / InputB;
+        }
+
+        /// <summary>
+        /// Dette er en metode til at få kvadratroden af ét input tal
+        /// </summary>
+        /// <param name="InputA"></param>
+        /// <returns></returns>
+        public double Sqrt(double InputA)
+        {
+            return Math.Sqrt(InputA);
+        }
+
+
+    }
+}
---
+++ SharedCalculatorTests/LommeregnerTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator.Tests
+{
+    [TestClass()]
+    public class LommeregnerTests
+    {
+        [TestMethod()]
+        public void AdditionTest()
+        {
+            //Arrange
+            Lommeregner lm = new Lommeregner();
+
+            //Act
+            double result = lm.Addition(2, 2);
+
+            //Assert
+            Assert.AreEqual(4, result);
+
+        }
+
+        [TestMethod()]
+        public void SubtractionTest()
+        {
+            //Arrange
+            Lommeregner lm = new Lommeregner();
+
+            //Act
+            double result = lm.Subtraction(10, 1);
+
+            //Assert
+            Assert.AreEqual(9, result);
+
+        }
+
+        [TestMethod()]
+        public void MultiplicationTest()
+        {
+            //Arrange
+            Lommeregner lm = new Lommeregner();
+
+            //Act
+            double result = lm.Multiplication(3, 3);
+
+            //Assert
+            Assert.AreEqual(9, result);
+
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            //Arrange
+            Lommeregner lm = new Lommeregner();
+
+            //Act
+            double result = lm.Division(4, 2);
+
+            //Assert
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod()]
+        public void SqrtTest()
+        {
+            //Arrange
+            Lommeregner lm = new Lommeregner();
+
+            //Act
+            double result = lm.Sqrt(9);
+
+            //Assert
+            Assert.AreEqual(3, result);
+
+        }
+    }
+}
